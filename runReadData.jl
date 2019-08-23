561e5f77dab7422088e0c628f51d3ba498b8453c
hyzhou <hyzhou@umich.edu>
2019-08-23 09:33:10 -0400

    Convenient way to readdata?

---
+++ runReadData.jl
@@ -0,0 +1,11 @@
+# Script for using VisAna in REPL
+#
+# Hongyang Zhou, hyzhou@umich.edu 08/02/2019
+
+# Check if filename
+
+# Check if present npict
+
+# If not, read in npict from command line
+
+filehead, data, filelist = readdata(filename);
