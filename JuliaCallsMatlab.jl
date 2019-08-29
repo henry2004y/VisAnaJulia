cac98e4e5925a727502842bf5d54a2e741ce4b07
henry_2004y <hyzhou@umich.edu>
2019-08-29 09:57:06 -0400

    Added more usages of calling Matlab.

--- JuliaCallsMatlab.jl
+++ JuliaCallsMatlab.jl
@@ -1,7 +1,7 @@
 # Calling Matlab functions in Julia.
 #
-# This requires that Matlab can be called from the command line.
-#
+# This requires that Matlab can be called from the command line, which means the
+# MATLAB_PATH should be properly set.
 # Hongyang Zhou, hyzhou@umich.edu 07/17/2019
 
 using MATLAB
@@ -14,3 +14,29 @@ filehead, data = mxcall(:read_data,2,filename);
 data = data["file1"];
 
 mat"plot_data($data,$filehead,'p','plotmode','contbar')"
+
+
+x = -2:0.2:2
+y = x'
+z = x .* exp.(-x.^2 .- y.^2)
+x = collect(x)
+y = collect(y)
+@mput x
+@mput y
+eval_string("z = x .* exp(-x.^2 - y.^2)")
+px, py = mxcall(:gradient, 2, z)
+
+quiver(x,y,px,py)
+mat"""
+contour($x,$y,$z)
+hold on
+quiver($x,$y,$px,$py)
+hold off
+"""
+
+# Compare the Matlab plotting to the matplotlib one.
+using PyPlot
+x = -2:0.2:2
+y = x'
+contour(x,y,z)
+quiver(x,y,px,py)
