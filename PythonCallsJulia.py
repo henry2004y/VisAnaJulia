29a4fff443060541e505767cf334c4a78f9e7b8f
hyzhou <hyzhou@umich.edu>
2019-09-11 13:59:18 -0400

    Added comments.

--- PythonCallsJulia.py
+++ PythonCallsJulia.py
@@ -5,3 +5,7 @@ j.include('VisAna.jl')
 readdata = j.eval('VisAna.readdata')
 filename='1d_bin.out'
 filehead, data, filelist = readdata(filename,verbose=False);
+
+# It turns out that I was too optimistic about using Python here. Even is the
+# function calls are working, the data was saved as jlwrap object, which I
+# cannot do any operation on it in Python.
