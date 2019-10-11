2f8730b293bcc90eac9314c3c42e046105c12aea
hyzhou <hyzhou@umich.edu>
2019-10-10 23:45:58 -0400

    Added concatenation in Julia instead of calling pTEC.

--- src/convert2vtk.jl
+++ src/convert2vtk.jl
@@ -1,23 +1,40 @@
+#!/usr/bin/env julia --startup-file=no
 # Script for converting BATSRUS unstructured binary outputs to VTK formats.
 # Example usage:
 # julia -p 8 convert_parallel.jl
 #
-# It is assumed that this script is executed from run directory. If not, please
-# specify the relative location of the output files to be processed.
+# Currently the relative location of the output files to be processed is "IO2".
 #
 # Hongyang Zhou, hyzhou@umich.edu
 
 using Distributed
-@everywhere using VisAna, Glob
+using Pkg
+if "VisAna" ∈ keys(Pkg.installed())
+   @everywhere using VisAna
+else
+   @warn "VisAna not installed. Consider installing the package by
+   `Pkg.add(PackageSpec(url=\"https://github.com/henry2004y/VisAnaJulia\",
+    rev=\"master\"))`"
+end
+@everywhere using Glob
 
-catcommand = `./pTEC`
-run(catcommand)
+dir = "IO2"
 
-filenamesIn = "3d*.dat"
-dir = "GM/IO2"
+# Concatenate files
+headers = glob("*.T", dir)
+@sync @distributed for header in headers
+   name = basename(header)[1:end-2]
+   println("header=$(header)")
+   nameIn = glob(name*"*.tec", dir)
+   nameOut= joinpath(dir, name*".dat")
+   run(pipeline(`cat $(nameIn)`, nameOut))
+   rm(header)
+   rm.(nameIn)
+end
 
+filenamesIn = "*.dat"
 filenames = Vector{String}(undef,0)
-filesfound = glob(filenamesIn)
+filesfound = glob(filenamesIn, dir)
 filenames = vcat(filenames, filesfound)
 # Do not work on files that have already been converted
 filenames = [fname for fname in filenames if ~isfile(fname[1:end-3]*"vtu")]
@@ -29,3 +46,10 @@ filenames = [fname for fname in filenames if ~isfile(fname[1:end-3]*"vtu")]
 end
 
 # Choose whether or not to delete the original *.dat files.
+#datFiles = glob("*.dat", dir)
+# Parallel:
+#@sync @distributed for file in datFiles
+#   rm(file)
+#end
+# Serial (might be faster if I/O bound):
+#rm.(datfiles)
