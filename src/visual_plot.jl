46ac9e13c49963c88890c3e97cd729ffc3e992e9
henry_2004y <hyzhou@umich.edu>
2019-12-03 23:29:19 -0500

    Remove duplicate stuff; only compare lower case strings.

--- src/visual_plot.jl
+++ src/visual_plot.jl
@@ -18,7 +18,9 @@ pyplot()
    ndim = filehead[:ndim]
 
    x,w = data.x, data.w
-   VarIndex_ = findfirst(x->x==var,filehead[:wnames])
+   VarIndex_ = findfirst(x->x==lowercase(var),
+      lowercase.(filehead[:wnames]))
+   isempty(VarIndex_) && error("$(var) not found in header variables!")
 
    if ndim == 1
       y = w[:,VarIndex_]
@@ -83,76 +85,6 @@ pyplot()
          xi, yi, wi
       end
    end
-end
-
-# Type annotations on keyword arguments not currently supported in recipes.
-# Type information has been discarded
-#=
-@recipe function contourf(data::Data, filehead::Dict, var::String,
-	levels::Int=0,
-   plotrange::Vector{Float64}=[-Inf,Inf,-Inf,Inf], plotinterval::Float64=0.1)
-
-   xi, yi, wi = getdata(data, filehead, var, plotrange, plotinterval)
-
-	return xi, yi, wi
-end
-=#
-
-"""Prepare data for passing to plotting functions."""
-function getdata(data, filehead, var, plotrange, plotinterval)
-   x,w = data.x, data.w
-   ndim = filehead[:ndim]
-
-   VarIndex_ = findfirst(x->x==lowercase(var), lowercase.(filehead[:wnames]))
-   isempty(VarIndex_) && error("$(var) not found in header variables!")
-
-   if filehead[:gencoord] # Generalized coordinates
-      X = vec(x[:,:,1])
-      Y = vec(x[:,:,2])
-      W = vec(w[:,:,VarIndex_])
-
-      if any(abs.(plotrange) .== Inf)
-         if plotrange[1] == -Inf plotrange[1] = minimum(X) end
-         if plotrange[2] ==  Inf plotrange[2] = maximum(X) end
-         if plotrange[3] == -Inf plotrange[3] = minimum(Y) end
-         if plotrange[4] ==  Inf plotrange[4] = maximum(Y) end
-      end
-
-      # Create grid values first.
-      xi = range(plotrange[1], stop=plotrange[2], step=plotinterval)
-      yi = range(plotrange[3], stop=plotrange[4], step=plotinterval)
-      # Perform linear interpolation of the data (x,y) on grid(xi,yi)
-      triang = matplotlib.tri.Triangulation(X,Y)
-      interpolator = matplotlib.tri.LinearTriInterpolator(triang, W)
-      Xi = [y for x in xi, y in yi]
-	   Yi = [x for x in xi, y in yi]
-      wi = interpolator(Xi, Yi)
-   else # Cartesian coordinates
-      if all(isinf.(plotrange))
-         xi = x[:,:,1]
-         yi = x[:,:,2]
-         wi = w[:,:,VarIndex_]
-      else
-		   X = x[:,1,1]
-		   Y = x[1,:,2]
-         if plotrange[1] == -Inf plotrange[1] = minimum(X) end
-         if plotrange[2] ==  Inf plotrange[2] = maximum(X) end
-         if plotrange[3] == -Inf plotrange[3] = minimum(Y) end
-         if plotrange[4] ==  Inf plotrange[4] = maximum(Y) end
-
-         W = w[:,:,VarIndex_]
-
-         xi = range(plotrange[1], stop=plotrange[2], step=plotinterval)
-         yi = range(plotrange[3], stop=plotrange[4], step=plotinterval)
-
-         spline = Spline2D(X, Y, W)
-         Xi = [i for i in xi, j in yi]
-         Yi = [j for i in xi, j in yi]
-         wi = spline(Xi[:], Yi[:])
-         wi = reshape(wi, size(Xi))'
-      end
-   end
-   return xi, yi, wi
 end
 
 # that was pretty easy!
