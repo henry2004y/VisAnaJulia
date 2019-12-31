1357272363049f9b006419275e5159149597aa43
hyzhou <hyzhou@umich.edu>
2019-12-31 15:38:20 -0500

    More attempts on CI.

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,12 @@ using Documenter, VisAna
 
 push!(LOAD_PATH,"../src/")
 
-makedocs(sitename="VisAna Documentation")
+makedocs(
+    sitename="VisAna Documentation",
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true"
+    )
+)
 
 deploydocs(
     repo = "github.com/henry2004y/VisAnaJulia",
