2985815a56930b3ccf0e5ced4c271c2eaf1cb8bb
hyzhou <hyzhou@umich.edu>
2019-12-31 18:09:39 -0500

    minor changes.

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,16 @@
 using Documenter, VisAna
 
-push!(LOAD_PATH,"../src/")
+#push!(LOAD_PATH,"../src/")
 
 makedocs(
-    sitename="VisAna Documentation",
+    sitename="VisAna",
     format = Documenter.HTML(
         prettyurls = get(ENV, "CI", nothing) == "true"
     )
 )
 
 deploydocs(
-    repo = "github.com/henry2004y/VisAnaJulia",
+    repo = "github.com/henry2004y/VisAnaJulia.git",
     target = "build",
     branch = "gh-pages"
 )
