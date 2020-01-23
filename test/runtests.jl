f5cbe2f43050dc0cd10dd4b1c641eda088735d04
hyzhou <hyzhou@umich.edu>
2020-01-23 16:14:28 -0500

    Update tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,11 +8,11 @@ using VisAna, PyPlot, Test
 
 @testset "reading 1D ascii" begin
    filename = "1d__raw_2_t25.60000_n00000258.out"
-   filehead, data, filelist = readdata(filename)
-   @test isa(filehead[1], Dict)
+   head, data, list = readdata(filename, verbose=true)
+   @test isa(head[1], Dict)
    @test isa(data[1], Data)
-   @test isa(filelist[1], FileList)
-   plotdata(data[1],filehead[1],"p",plotmode="line")
+   @test isa(list[1], FileList)
+   plotdata(data[1],head[1],"p",plotmode="line")
    line = gca().lines[1]
    @test line.get_xdata() ≈ data[1].x
    @test line.get_ydata() ≈ data[1].w[:,10]
@@ -20,39 +20,39 @@ end
 
 @testset "reading 2D structured binary" begin
    filename = "z=0_raw_1_t25.60000_n00000258.out"
-   filehead, data, filelist = readdata(filename)
-   @test isa(filehead[1], Dict)
+   head, data, list = readdata(filename)
+   @test isa(head[1], Dict)
    @test isa(data[1], Data)
-   @test isa(filelist[1], FileList)
-   plotdata(data[1],filehead[1],"p bx;by",plotmode="contbar streamover")
+   @test isa(list[1], FileList)
+   plotdata(data[1],head[1],"p bx;by",plotmode="contbar streamover")
    ax = gca()
    @test isa(ax, PyPlot.PyObject)
-   contourf(data[1],filehead[1],"p")
+   contourf(data[1],head[1],"p")
    ax = gca()
    @test isa(ax, PyPlot.PyObject)
 end
 
 @testset "reading 2D unstructured binary" begin
    #filename = "z=0_raw_1_t25.60000_n00000258.out"
-   #filehead, data, filelist = readdata(filename)
+   #head, data, list = readdata(filename)
 end
 
 @testset "reading 3D structured binary" begin
    #filename = "3d_structured.out";
-   #filehead, data, filelist = readdata(filename,verbose=false);
+   #head, data, list = readdata(filename,verbose=false);
 end
 
 @testset "log" begin
    logfilename = "log_n000001.log"
-   filehead, data = readlogdata(logfilename)
-   @test isa(filehead, Dict)
+   head, data = readlogdata(logfilename)
+   @test isa(head, Dict)
    @test isa(data, Array)
 end
 
 @testset "vtk" begin
    @info("VTK conversion test.")
    filename = "3d_bin.dat"
-   head, data, connectivity  = readtecdata(filename, true)
+   head, data, connectivity  = readtecdata(filename, IsBinary=true)
    @test maximum(connectivity) ≤ head[:nNode] # check if it's read correctly
    convertVTK(head, data, connectivity)
    @test isfile("3DBATSRUS.vtu")
