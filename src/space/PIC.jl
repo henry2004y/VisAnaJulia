49bc1cdddc85b1823e84826fe64e47a4fb8d2882
hyzhou <hyzhou@umich.edu>
2020-02-07 10:14:26 -0500

    Added new script for postprocessing PIC box outputs.

---
+++ src/space/PIC.jl
@@ -0,0 +1,17 @@
+# Plotting PIC variables from box outputs
+#
+# Hongyang Zhou, hyzhou@umich.edu 02/06/2020
+
+using VisAna
+
+dir = "/Users/hyzhou"
+fnameField = "3d_var_region0_0_t00001640_n00020369.out"
+
+head, data = readdata(fnameField)
+
+# B, E, V, J, n, P
+
+# non-gyrotropy index Dng
+
+
+# Dissipation measure De
