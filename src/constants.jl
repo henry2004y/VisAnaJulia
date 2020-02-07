1dcd03c28b513698b778cc57ac3462077cb3df43
hyzhou <hyzhou@srbwks2015-0049.engin.umich.edu>
2020-02-07 12:38:18 -0500

    Added more.

--- src/constants.jl
+++ src/constants.jl
@@ -7,3 +7,6 @@ const Rm = 2444000. #[m], Mercury's radius
 
 const ionElectronMassRatio = 100 # ion-electron mass ratio in PIC simulation
 const ionMass = 14 # single fluid ion mass in Ganymede's simulation
+
+const me = 9.10938356e10-31 #[kg]
+const mp = 1.6726219e10-27  #[kg]
