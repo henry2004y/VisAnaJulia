d1736926aec009c8dd77fda9f7d54bbb91a92a35
hyzhou <hyzhou@srbwks2015-0049.engin.umich.edu>
2020-02-10 16:50:07 -0500

    Attempt on temperature plots.

--- src/space/PIC.jl
+++ src/space/PIC.jl
@@ -29,11 +29,14 @@ me = head[1][:eqpar][1]
 qe = head[1][:eqpar][2]
 mi = head[1][:eqpar][3]
 qi = head[1][:eqpar][4]
+const kB = 1.38064852e-23 # [m^2 kg s^-2 K^-1]
 const vAlfven = 253. # reference Alfven velocity, [km/s]
 const B₀ = √((-10.)^2+(-6.)^2+(-86.)^2)
 const E₀ = vAlfven*B₀ # [μV/m]
 const ρ₀ = 56.0     # [amu/cc]
 const J₀ = 4.0*vAlfven
+#const T₀ = vAlfven^2
+const T₀ = 0.2/4 # Pe/n₀
 
 plotrange = [-2.05, -1.75, -0.5, 0.5]
 #plotrange=[-Inf, Inf, -Inf, Inf]
@@ -81,7 +84,7 @@ X, Z, Pxze = cutdata(data[1],head[1],"pXZS0",cut='y',cutPlaneIndex=cI,
 	plotrange=plotrange)
 X, Z, Pyze = cutdata(data[1],head[1],"pYZS0",cut='y',cutPlaneIndex=cI,
 	plotrange=plotrange)
-
+#=
 X, Z, Pxxi = cutdata(data[1],head[1],"pXXS1",cut='y',cutPlaneIndex=cI,
 	plotrange=plotrange)
 X, Z, Pyyi = cutdata(data[1],head[1],"pYYS1",cut='y',cutPlaneIndex=cI,
@@ -94,6 +97,7 @@ X, Z, Pxzi = cutdata(data[1],head[1],"pXZS1",cut='y',cutPlaneIndex=cI,
 	plotrange=plotrange)
 X, Z, Pyzi = cutdata(data[1],head[1],"pYZS1",cut='y',cutPlaneIndex=cI,
 	plotrange=plotrange)
+=#
 
 x  = X[:,1]
 z  = Z[1,:]
@@ -160,85 +164,71 @@ for i = 1:length(xstart)
    gridType="ndgrid")
 end
 
-ax[1].contour(Z,X,Bz,[0.],colors="k",linestyles="dotted",linewidths=1.)
 ax[1].annotate(L"B_z", xy=labelPos, xycoords="axes fraction",color="w")
 
 # By
 c = ax[2].contourf(Z,X,By./B₀,levels, norm=DN(0)) #
 colorbar(c, cax=axin[2])
-ax[2].contour(Z,X,Bz,[0.],colors="k",linestyles="dotted",linewidths=1.)
 ax[2].annotate(L"B_y", xy=labelPos, xycoords="axes fraction")
 
 # Ex
 c = ax[3].contourf(Z,X,Ex./E₀,levels, norm=DN(0))
 colorbar(c, cax=axin[3])
-ax[3].contour(Z,X,Bz,[0.],colors="k",linestyles="dotted",linewidths=1.)
 ax[3].annotate(L"E_x", xy=labelPos, xycoords="axes fraction")
 
 # Uzi
 c = ax[4].contourf(Z,X,Uzi./vAlfven,levels, norm=DN(0))
 colorbar(c, cax=axin[4])
-ax[4].contour(Z,X,Bz,[0.],colors="k",linestyles="dotted",linewidths=1.)
 ax[4].annotate(L"v_{iz}", xy=labelPos, xycoords="axes fraction")
 
 # Uyi
 c = ax[5].contourf(Z,X,Uyi./vAlfven,levels, norm=DN(0))
 colorbar(c, cax=axin[5])
-ax[5].contour(Z,X,Bz,[0.],colors="k",linestyles="dotted",linewidths=1.)
 ax[5].annotate(L"v_{iy}", xy=labelPos, xycoords="axes fraction", color="w")
 
 # Uze
 c = ax[6].contourf(Z,X,Uze./vAlfven,levels, norm=DN(0))
 colorbar(c, cax=axin[6])
-ax[6].contour(Z,X,Bz,[0.],colors="k",linestyles="dotted",linewidths=1.)
 ax[6].annotate(L"v_{ez}", xy=labelPos, xycoords="axes fraction")
 
 # Uye
 c = ax[7].contourf(Z,X,Uye./vAlfven,levels, norm=DN(0))
 colorbar(c, cax=axin[7])
-ax[7].contour(Z,X,Bz,[0.],colors="k",linestyles="dotted",linewidths=1.)
 ax[7].annotate(L"v_{ey}", xy=labelPos, xycoords="axes fraction")
 
 # ρi
 c = ax[8].contourf(Z,X,ρi./ρ₀,levels, cmap="jet")
 colorbar(c, cax=axin[8])
-ax[8].contour(Z,X,Bz,[0.],colors="k",linestyles="dotted",linewidths=1.)
 ax[8].annotate(L"\rho_i", xy=labelPos, xycoords="axes fraction",color="w")
 
 # Jz
 c = ax[9].contourf(Z,X,Jz,levels, norm=DN(0))
 colorbar(c, cax=axin[9])
-ax[9].contour(Z,X,Bz,[0.],colors="k",linestyles="dotted",linewidths=1.)
 ax[9].annotate(L"J_z", xy=labelPos, xycoords="axes fraction")
 
 # Jy
 c = ax[10].contourf(Z,X,Jy,levels, norm=DN(0))
 colorbar(c, cax=axin[10])
-ax[10].contour(Z,X,Bz,[0.],colors="k",linestyles="dotted",linewidths=1.)
 ax[10].annotate(L"J_y", xy=labelPos, xycoords="axes fraction")
 
 # Deviation from ideal MHD
 c = ax[11].contourf(Z,X, (Ex.+Uyi.*Bz.-Uzi.*By)./E₀, levels, norm=DN(0))
 colorbar(c, cax=axin[11])
-ax[11].contour(Z,X,Bz,[0.],colors="k",linestyles="dotted",linewidths=1.)
 ax[11].annotate(L"(E+v_i\times B)_x", xy=labelPos, xycoords="axes fraction")
 
 # Deviation from Hall MHD
 c = ax[12].contourf(Z,X, (Ex.+Uye.*Bz.-Uze.*By)./E₀, levels, norm=DN(0))
 colorbar(c, cax=axin[12])
-ax[12].contour(Z,X,Bz,[0.],colors="k",linestyles="dotted",linewidths=1.)
 ax[12].annotate(L"(E+v_e \times B)_x", xy=labelPos, xycoords="axes fraction")
 
 # Deviation from ideal MHD
 c = ax[13].contourf(Z,X, (Ey.+Uzi.*Bx.-Uxi.*Bz)./E₀, levels, norm=DN(0))
 colorbar(c, cax=axin[13])
-ax[13].contour(Z,X,Bz,[0.],colors="k",linestyles="dotted",linewidths=1.)
 ax[13].annotate(L"(E+v_i\times B)_y", xy=labelPos, xycoords="axes fraction")
 
 # Deviation from Hall MHD
 c = ax[14].contourf(Z,X, (Ey.+Uze.*Bx.-Uxe.*Bz)./E₀, levels, norm=DN(0))
 colorbar(c, cax=axin[14])
-ax[14].contour(Z,X,Bz,[0.],colors="k",linestyles="dotted",linewidths=1.)
 ax[14].annotate(L"(E+v_e\times B)_y", xy=labelPos, xycoords="axes fraction")
 
 # non-gyrotropy index Dng (for electron, not for electron+ion!)
@@ -247,7 +237,6 @@ Dng = @. 2*√(Pxye^2 + Pxze^2 + Pyze^2) / (Pxxe + Pyye + Pzze)
 c = ax[15].contourf(Z,X,Dng, levels, cmap="jet")
 
 colorbar(c, cax=axin[15])
-ax[15].contour(Z,X,Bz,[0.],colors="k",linestyles="dotted",linewidths=1.)
 ax[15].annotate(L"D_{ng}", xy=labelPos, xycoords="axes fraction", color="w")
 
 
@@ -259,7 +248,6 @@ Dₑ = @. (Jx*(Ex + Uye*Bz - Uze*By) +
 
 c = ax[16].contourf(Z,X,Dₑ, levels, norm=DN(0))
 colorbar(c, cax=axin[16])
-ax[16].contour(Z,X,Bz,[0.],colors="k",linestyles="dotted",linewidths=1.)
 ax[16].annotate(L"D_e", xy=labelPos, xycoords="axes fraction")
 
 
@@ -267,6 +255,7 @@ for i in 1:length(ax)
    if i < 15
       [ax[i].plot(zl[j],xl[j],"-",color="k",lw=0.4) for j in 1:length(xstart)]
    end
+   ax[i].contour(Z,X,Bz,[0.],colors="k",linestyles="dotted",linewidths=1.)
    ax[i].set_aspect("equal", "box")
    ax[i].invert_yaxis()
    if i != 8 && i != 16
@@ -281,3 +270,43 @@ end
 #tight_layout()
 
 # T∥ and T⟂
+# vec0 = (x0, y0, z0) = (0,1,0)
+# vec1 = (x1, y1, z1) = (bx, by, bz)/b
+# vec2 = (x2, y2, y2) = vec0 x vec1 / norm
+# vec3 = (x3, y3, z3) = vec1 x vec2
+
+B = @. √(Bx^2 + By^2 + Bz^2)
+x0, y0, z0 = 0.0, 1.0, 0.0
+x1, y1, z1 = Bx./B, By./B, Bz./B
+x2, y2, z2 = @. y0*z1-z0*y1, z0*x1-x0*z1, x0*y1-y0*x1
+z3 = @. sqrt(x2^2 + y2^2 + z2^2)
+x2, y2, z2 = @. x2/z3, y2/z3, z2/z3
+x3, y3, z3 = @. y1*z2-z1*y2, z1*x2-x1*z2, x1*y2-y1*x2
+
+P11e = @. Pxxe*x1^2+Pyye*y1^2+Pzze*z1^2+2*(Pxye*x1*y1+Pxze*x1*z1+Pyze*y1*z1)
+P22e = @. Pxxe*x2^2+Pyye*y2^2+Pzze*z2^2+2*(Pxye*x2*y2+Pxze*x2*z2+Pyze*y2*z2)
+P33e = @. Pxxe*x3^2+Pyye*y3^2+Pzze*z3^2+2*(Pxye*x3*y3+Pxze*x3*z3+Pyze*y3*z3)
+#P12e = @. Pxxe*x1*x2+Pyye*y1*y2+Pzze*z1*z2+Pxye*(x1*y2+y1*x2)+Pxze*(x1*z2+z1*x2)+Pyze*(y1*z2+z1*y2)
+#P13e = @. Pxxe*x1*x3+Pyye*y1*y3+Pzze*z1*z3+Pxye*(x1*y3+y1*x3)+Pxze*(x1*z3+z1*x3)+Pyze*(y1*z3+z1*y3)
+#P23e = @. Pxxe*x2*x3+Pyye*y2*y3+Pzze*z2*z3+Pxye*(x2*y3+y2*x3)+Pxze*(x2*z3+z2*x3)+Pyze*(y2*z3+z2*y3)
+
+Tpar = P11e./ρe.*me
+Tperp = @. √(P22e^2 + P33e^2)./ρe.*me
+
+figure()
+ax1 = subplot(211)
+contourf(Z,X, Tpar./T₀, levels, cmap="viridis"); colorbar()
+clim([0.0,6.0])
+contour(Z,X,Bz,[0.],colors="k",linestyles="dotted",linewidths=1.)
+ax1.invert_yaxis()
+title(L"T_e\parallel")
+ax1.axes.xaxis.set_ticklabels([])
+ax2 = subplot(212)
+contourf(Z,X, Tperp./T₀, levels, cmap="viridis"); colorbar()
+clim([0.0,6.0])
+contour(Z,X,Bz,[0.],colors="k",linestyles="dotted",linewidths=1.)
+ax2.invert_yaxis()
+title(L"T_e\perp")
+
+#figure()
+#contourf(X,Z,Tpar./Tperp, levels); colorbar()
