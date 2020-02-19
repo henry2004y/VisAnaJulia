14aa893f79a35101001109e64fb519a2f388dbfc
hyzhou <hyzhou@umich.edu>
2020-02-19 09:53:21 -0500

    Fix me, mp value; added more.

--- src/constants.jl
+++ src/constants.jl
@@ -8,5 +8,9 @@ const Rm = 2444000. #[m], Mercury's radius
 const ionElectronMassRatio = 100 # ion-electron mass ratio in PIC simulation
 const ionMass = 14 # single fluid ion mass in Ganymede's simulation
 
-const me = 9.10938356e10-31 #[kg]
-const mp = 1.6726219e10-27  #[kg]
+const me = 9.10938356e-31 #[kg]
+const mp = 1.6726219e-27  #[kg]
+
+const c = 3e8 #[m/s]
+const Rₑ = 6.38e6 #[m]
+const μ = 4*π*1e-7  # Vacuum permeability, [V*s/(A*m)]
