af4358783763ed74643f39676bb5d05fd08ce91e
Hongyang Zhou <hyzhou@umich.edu>
2020-07-28 12:50:20 -0400

    Fixed indentation.

--- src/dipole.jl
+++ src/dipole.jl
@@ -14,9 +14,9 @@ For a position `x`, `y` in units of planetary radius, return the strength of the
 dipole magnetic field in nT.
 """
 function b_mag(x, y)
-	r = @. √(x^2 + y^2)
-	cosy = y./r
-	B = @. 30400. * sqrt(1+3*cosy^2)/r^3
+   r = @. √(x^2 + y^2)
+   cosy = y./r
+   B = @. 30400. * sqrt(1+3*cosy^2)/r^3
 end
 
 """
@@ -27,26 +27,26 @@ y components of b_hat for a dipole field. The grid is organized in meshgrid
 """
 function b_hat(x, y; gridType="meshgrid")
    if gridType == "meshgrid"
-	   xgrid = [i for j in y, i in x]
+      xgrid = [i for j in y, i in x]
       ygrid = [j for j in y, i in x]
    else
       xgrid = [i for i in x, j in y]
       ygrid = [j for i in x, j in y]
    end
 
-	r = @. sqrt(xgrid^2 + ygrid^2)
-	cosy = ygrid./r
-	sinx = xgrid./r
+   r = @. sqrt(xgrid^2 + ygrid^2)
+   cosy = ygrid./r
+   sinx = xgrid./r
 
-	denom = @. sqrt(1.0 + 3.0*cosy^2)
+   denom = @. sqrt(1.0 + 3.0*cosy^2)
 
-	br = @. 2.0 * cosy / denom
-	bθ =          sinx ./ denom
+   br = @. 2.0 * cosy / denom
+   bθ =          sinx ./ denom
 
-	bx = @. br*sinx + bθ*cosy
-	by = @. br*cosy - bθ*sinx
+   bx = @. br*sinx + bθ*cosy
+   by = @. br*cosy - bθ*sinx
 
-	return bx, by
+   return bx, by
 end
 
 """
@@ -56,27 +56,27 @@ For a starting X, Y point return x and y vectors that trace the dipole field
 line that passes through the given point.
 """
 function b_line(x, y; npoints=30)
-	r = sqrt(x^2 + y^2)
-	try
-		theta = atan(x/y)
-	catch
-		@warn "ZeroDivisionError"
-		theta = pi/2.0
-	end
+   r = sqrt(x^2 + y^2)
+   try
+      theta = atan(x/y)
+   catch
+      @warn "ZeroDivisionError"
+      theta = pi/2.0
+   end
 
-	R = r/(sin(theta)^2)
+   R = r/(sin(theta)^2)
 
-	if x < 0
-		theta = π:π/npoints:2.0π
-	else
-		theta = 0:π/npoints:π
-	end
+   if x < 0
+      theta = π:π/npoints:2.0π
+   else
+      theta = 0:π/npoints:π
+   end
 
-	r_vec = @. R * sin(theta)^2
-	x_out = @. r_vec * sin(theta)
-	y_out = @. r_vec * cos(theta)
+   r_vec = @. R * sin(theta)^2
+   x_out = @. r_vec * sin(theta)
+   y_out = @. r_vec * cos(theta)
 
-	return x_out, y_out
+   return x_out, y_out
 end
 
 "A quick test of the dipole field functions."
