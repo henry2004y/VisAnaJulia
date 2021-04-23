8d525563a3f2a6583a8539b37b42108e19790e68
Hongyang Zhou <hyzhou@umich.edu>
2021-04-23 22:21:30 +0300

    Add Parker spiral plots

--- space/constants.jl
+++ space/constants.jl
@@ -15,3 +15,19 @@ const μ₀ = 4π*1e-7          # Vacuum permeability, [H/m]
 
 const Rₑ = 6.38e6           # Earth radius, [m]
 const Rm = 2444000.         # Mercury's radius, [m]
+
+# Solar
+const Tsolarrotate = 25.05*24*3600 # [s]
+const ωsun = 2π / Tsolarrotate # [rad/s]
+const Rsun = 696340e3 # solar radius, [m]
+const AU = 1.496e11 # sun-Earth distance, [m]
+
+# Distance from sun
+# https://www.enchantedlearning.com/subjects/astronomy/planets/
+const dMercury = 0.39  # [AU], 57.91e9 [m]
+const dVenus   = 0.723 # [AU], 108.2e9 [m]
+const dMars    = 1.524 # [AU], 227.9e9 [m]
+const dJupiter = 5.203 # [AU], 778.5e9 [m]
+const dSaturn  = 9.539 # [AU], 1434e9  [m]
+const dUranus  = 19.18 # [AU], 2871e9  [m]
+const dNeptune = 30.06 # [AU], 4495e9  [m]
